7477fadb92133a4493dc4f84859942835b11f625
hirakouki <kouki6533@gmail.com>
2026-06-29 10:33:05 +0900

    fix:カラム関係の文言修正

--- db/migrations/20260626045435_create_event_members.sql
+++ db/migrations/20260626045435_create_event_members.sql
@@ -2,7 +2,7 @@
 CREATE TABLE event_members (
     id UUID PRIMARY KEY,
     event_id UUID NOT NULL   REFERENCES events(id),
-    profile_id UUID NOT NULL REFERENCES profiles(id),
+    profile_id UUID REFERENCES profiles(id),
     username TEXT NOT NULL,         
     mail_address TEXT NOT NULL
 );
