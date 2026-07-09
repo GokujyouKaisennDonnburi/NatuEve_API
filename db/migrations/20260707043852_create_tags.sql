f52d9914a1f91aae95dfaee3376b78817cb562f1
y_k <ka.20.zu.04@gmail.com>
2026-07-09 14:25:42 +0900

    update:tagsテーブルの修正

--- db/migrations/20260707043852_create_tags.sql
+++ db/migrations/20260707043852_create_tags.sql
@@ -2,7 +2,8 @@
 -- tags はイベントに付与するタグ。
 CREATE TABLE tags (
     id          UUID PRIMARY KEY,
-    name    VARCHAR(255) NOT NULL UNIQUE
+    name    VARCHAR(255) NOT NULL UNIQUE,
+    normalized_name VARCHAR(30) NOT NULL UNIQUE
 );
 
 -- +goose Down
