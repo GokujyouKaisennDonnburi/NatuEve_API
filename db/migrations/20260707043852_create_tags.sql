6321dc86e6acdf2447f64dd7e7957d12e9bdb154
y_k <ka.20.zu.04@gmail.com>
2026-07-10 10:10:34 +0900

    tagsテーブルの文字数制限間違いの解消

--- db/migrations/20260707043852_create_tags.sql
+++ db/migrations/20260707043852_create_tags.sql
@@ -2,8 +2,8 @@
 -- tags はイベントに付与するタグ。
 CREATE TABLE tags (
     id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    name    VARCHAR(255) NOT NULL UNIQUE,
-    normalized_name VARCHAR(30) NOT NULL UNIQUE
+    name    VARCHAR(30) NOT NULL UNIQUE,
+    normalized_name VARCHAR(255) NOT NULL UNIQUE
 );
 
 -- +goose Down
